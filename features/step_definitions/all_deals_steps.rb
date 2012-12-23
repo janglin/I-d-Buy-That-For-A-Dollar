8b539c0e717643cbc74ccb9f49dfaa84be3dd7dd
Levi Wilson <levi@leviwilson.com>
2012-12-23 00:39:53 -0500

    chooses the appropriate deal location

--- features/step_definitions/all_deals_steps.rb
+++ features/step_definitions/all_deals_steps.rb
@@ -1,7 +1,12 @@
-Given /^I would like to see all deals available to me in "([^"]*)"$/ do |which_area|
+Given /^I would like to see all deals available to me in "([^"]*), ([^"]*)"$/ do |city, state|
   visit_page(HomePage).go_to_sign_in
   on_page(SignInPage).sign_in_as_levi
   on_page(FeaturedDealPage).all_deals
+  on_page(AllDealsPage) do |page|
+    page.show_the_cities
+    page.platform.list_item_for(:id => "state_#{state[0].upcase}").click
+    page.platform.link_for(:text => /#{city}/).click
+  end
 end
 
 When /^all of the available deals have been displayed$/ do
---
+++ lib/pages/all_deals_page.rb
@@ -0,0 +1,5 @@
+class AllDealsPage
+  include PageObject
+
+  link(:show_the_cities, :href => "/cities")
+end
