f1c58043a635e06cba3c48d760ce6dc35187debd
Levi Wilson <levi@leviwilson.com>
2012-12-23 00:01:31 -0500

    initial cuke / RubyMine project

---
+++ features/support/env.rb
@@ -0,0 +1,11 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../../', 'lib'))
+
+require 'rspec-expectations'
+require 'page-object'
+require 'page-object/page_factory'
+require 'require_all'
+
+require_all 'lib'
+
+
+World(PageObject::PageFactory)
