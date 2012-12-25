be8cc5434d7bc3a59440d19eaf5fb9ccdf520df4
Levi Wilson <levi@leviwilson.com>
2012-12-24 22:26:56 -0500

    clean up Deal class

--- lib/elements/deal.rb
+++ lib/elements/deal.rb
@@ -6,25 +6,20 @@ class Deal
   end
 
   def categories
-    show_categories
-    element.lis.map &:text
+    all_categories.map &:text
   end
 
   def categorize_as(category)
-    show_categories
-    the_category = element.lis(:text => /#{category}/).first
-    the_category.a().click
+    categories_matching(category).first.click
   end
 
   def active_categories
-    show_categories
-    element.as(:class => "active").map &:title
+    all_active_categories.map &:title
   end
 
   def clear_active_categories
-    show_categories
-    element.as(:class => "active").each do |category|
-      category.click unless category.title.empty?
+    all_active_categories.each do |active_category|
+      active_category.click unless active_category.title.empty?
     end
   end
 
@@ -36,6 +31,23 @@ class Deal
   def show_categories
     hover_on_the_deal
     mouseover(element.element(:class => "heartx-heart"))
+  end
+
+  def all_categories(active_only=false)
+    show_categories
+    expected_class = "heartx-deal-feedback"
+    expected_class << " active" if active_only
+    element.as(:class => expected_class)
+  end
+
+  def all_active_categories
+    all_categories(true)
+  end
+
+  def categories_matching(text)
+    all_categories.select do |category|
+      category.title.match /#{text}/
+    end
   end
 
   def hover_on_the_deal
